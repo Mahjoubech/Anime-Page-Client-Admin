d869a77a37c1f34c344abae6dda2cd3951dace93
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-11-20 23:32:24 +0100

    add form ajouter anime html css and animation js

--- admin.js
+++ admin.js
@@ -2,3 +2,14 @@ let logout = document.getElementById('logout')
 logout.addEventListener('click',()=>{
     window.location.href = 'client.html';
 })
+const addAnimeBtn = document.querySelector('#anime-button');
+const addAnimeForm = document.querySelector('.add-anime-form');
+const cancelBtn = document.querySelector('.cancel-btn');
+
+addAnimeBtn.addEventListener('click', () => {
+  addAnimeForm.classList.remove('hidden');
+});
+
+cancelBtn.addEventListener('click', () => {
+  addAnimeForm.classList.add('hidden');
+});
